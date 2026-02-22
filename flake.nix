42646703e1980fbfe0f076e7d8a68ab24f46887d
J. Dekker <jdek@itanimul.li>
2026-02-22 16:59:01 +0100

    macos flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   inputs.flakelight.url = "github:nix-community/flakelight";
   outputs = { flakelight, ... }:
     flakelight ./. {
-      devShell.packages = pkgs: with pkgs; [ akkuPackages.scheme-langserver chez ];
+      systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
+      devShell.packages = pkgs: with pkgs; [ chez ];
       devShell.env = {
         CHEZSCHEMELIBDIRS = "./lib::.cache/lib";
       };
